e621b27fed842d5f372aa9893491eb2fc4a985fd
20172104250 <2418665930@qq.com>
2018-05-22 15:43:33 +0800

    Initial commit

---
+++ 米换算.cpp
@@ -0,0 +1,39 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+class CFeet
+{
+private:
+	int meters;
+	int feet;
+	double inches;
+public:
+	void init(int m, int f,  i)
+	{
+		m = meters;
+		f = feet;
+		i = inches;
+	}
+	void setmeters(int m)
+	{
+		feet = m*3.2808;
+		inches = (feet % 1.0)  *12;
+
+	}
+	void display()
+	{
+		cout << "feet" << feet << "inches" << inches << endl;
+	}
+
+};
+int main()
+{
+	CFeet ot;
+	ot.setmeters(15);
+	ot.display();
+	return 0;
+}
